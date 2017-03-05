590f9e623d3b314c73a32f44c36434799619d213
Kuan <Kuan@KONG-YOGA2.mobile.asu.edu>
2017-03-05 10:24:30 -0700

    This is the Max function.Under construction.

---
+++ JavaPractice/src/javapractice/Max.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javapractice;
+
+/**
+ * In this part, a Max function will be delivered.
+ * @author Kuan
+ */
+public class Max {
+    
+    
+}
