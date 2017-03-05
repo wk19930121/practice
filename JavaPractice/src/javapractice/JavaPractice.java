b37f37b71bf97c602a94d42bfaac4690432f680a
Kuan <Kuan@KONG-YOGA2.mobile.asu.edu>
2017-03-05 10:23:38 -0700

    The whole system is under construction.

---
+++ JavaPractice/src/javapractice/JavaPractice.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javapractice;
+
+/**
+ * This project is a practice of JAVA programming, what I will do is to realize 
+ * a set of mathematical calculations. 
+ * 
+ * The first function I will deliver is the Max function.
+ * 
+ * @author Kuan
+ */
+public class JavaPractice {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
