3bdc5b2219dbdec6aa63cbdef7dd72505d57001a
Kuan <Kuan@KONG-YOGA2.mobile.asu.edu>
2017-03-05 12:02:51 -0700

    This is the Max function.Under construction.

--- JavaPractice/src/javapractice/Max.java
+++ JavaPractice/src/javapractice/Max.java
@@ -10,7 +10,33 @@ package javapractice;
  * @author Kuan
  */
 public class Max {
-    public static void main (String[] arg){
-        
+    
+    double[] array; //This is the array to compare
+    int N;  //This is the length of the array
+    double maxValue; //This is the maximum value
+    
+    public Max(double[] A){
+        array = new double[A.length];
+        System.arraycopy(A, 0, array, 0, A.length);
+        N = A.length;
+    }
+    
+    public double max(){
+        for(int i=0;i<N-1;i++){
+            if (array[i]<array[i+1]){
+                maxValue = array[i+1];
+            }
+            else{
+                maxValue = array[i];
+            }
+        }
+        return maxValue;
+    }
+    
+    public static void main(String[] arg){
+        double[] testArray = {10,5,23,4,56,9};
+        Max MaxChange = new Max(testArray);
+        double maxV = MaxChange.max();
+        System.out.println("The maximum value in this array is "+maxV);
     }
 }
