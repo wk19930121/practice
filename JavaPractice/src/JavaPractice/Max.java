0ba99edb8c55a818e1583fb8c97607fc4bbd6b90
wk19930121 <wk19930121@gmail.com>
2017-03-05 20:16:32 -0700

    Completely reconstructed the project.

---
+++ JavaPractice/src/JavaPractice/Max.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javapractice;
+
+/**
+ * In this part, a Max function will be delivered.
+ * @author Kuan
+ */
+public class Max {
+    
+    double[] array; //This is the array to compare
+    double maxValue; //This is the maximum value
+    
+    public Max(double[] A){
+        
+        array = new double[A.length];   
+        System.arraycopy(A, 0, array, 0, A.length);
+        //Input is transfered to the local variable
+    }// This is construct
+    
+    public double getMax(){
+        for(int i=0;i<array.length-1;i++){
+            if (array[i]<array[i+1]){
+                maxValue = array[i+1];
+            }
+            else{
+                maxValue = array[i];
+            }
+        }
+        return maxValue;
+    }
+    
+
+}
