3b080ff9f74ab8801043d6d4d29b30f3668c7051
wk19930121 <wk19930121@gmail.com>
2017-03-05 21:20:57 -0700

    Problem about rabbits.

--- JavaPractice/src/JavaPractice/TestRabbit.java
+++ JavaPractice/src/JavaPractice/TestRabbit.java
@@ -10,8 +10,22 @@ package JavaPractice;
  * @author Kuan
  */
 public class TestRabbit {
+    
     public TestRabbit(){
         
-}
+    };
+    
+    // N is the month.
+    public static int sumRabbit(int N){
+        int k = 1;  //The quantity of the rabbits in the first month
+        int m = 0;
+        int temp;
+        for(int i=1; i < N; i++){
+            temp = k;
+            k = k + m;
+            m = temp;
+        }
+        return k;
+    }
     
 }
