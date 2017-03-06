a9b0244c7c7a4b6ecbe1bec3c19c85d8d67707cd
wk19930121 <wk19930121@gmail.com>
2017-03-05 20:16:25 -0700

    Completely reconstructed the project.

---
+++ JavaPractice/src/JavaPractice/JavaPractice.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javapractice;
+
+/**
+ * This project is a practice of JAVA programming, what I will do is to realize 
+ * a set of mathematical calculations. 
+ * 
+ * The first function I will deliver is the Max function.
+ * The first Max function is delivered.
+ * @author Kuan
+ */
+public class JavaPractice {
+
+    public static void main(String[] arg){
+        double[] testArray = {10,5,23,4,56,9};
+        Max MaxChange = new Max(testArray);
+        double maxV = MaxChange.getMax();
+        System.out.println("The maximum value in this array is "+maxV);
+    }
+    
+}
