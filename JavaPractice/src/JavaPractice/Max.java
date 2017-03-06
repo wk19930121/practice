57006338cb0c2eab56ea7ad69ada6b16089e0ce4
wk19930121 <wk19930121@gmail.com>
2017-03-06 00:01:25 -0700

    Problem about rabbits.

--- JavaPractice/src/JavaPractice/Max.java
+++ JavaPractice/src/JavaPractice/Max.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package javapractice;
+package JavaPractice;
 
 /**
  * In this part, a Max function will be delivered.
